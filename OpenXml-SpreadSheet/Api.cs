3dc2558f4780069bcffbd41b973f00e0ccfa780f
Ali reza Karimi <karimiformlfco@hotmail.com>
2023-11-17 11:30:44 +0330

    resolve null

--- OpenXml-SpreadSheet/Api.cs
+++ OpenXml-SpreadSheet/Api.cs
@@ -24,9 +24,28 @@ namespace OpenXml_SpreadSheet
             string content = await response.Content.ReadAsStringAsync();
             //string myResult = $"{{\"operationResult\":1,\"operation\":\"MemberStatusInquery\",\"responseDate\":\"2023-11-07 08:46:22\",\"personCode\":\"12586933\",\"lastName\":\"ايمانيان مزجين\",\"firstName\":\"صديقه\",\"areaCode\":\"5702\",\"areaName\":\"زنجان ناحيه - 2\",\"fatherName\":\"عربعلي\",\"nationalCode\":\"1639095349\",\"idNumber\":\"153\",\"issuePlace\":\"خلخال\",\"birthDate\":\"580401\",\"sex\":\"زن\",\"marriage\":\"متاهل\",\"employmentType\":\"رسمي\",\"employmentDate\":\"13800701\",\"totalState\":\"7\",\"settlemntAccountNumber\":\"0105397482009\",\"settlemntBank\":\"ملي\",\"membershipDate\":\"13830630\",\"countTransaction\":\"224\",\"lastPercent\":\"3\"}}";
 
-            Dictionary<string, string> dictionary =
-                JsonConvert.DeserializeObject<Dictionary<string, string>>(content);
 
+            //var res = new Dictionary<string, string>()
+            //                            {
+            //                                {".cbfmd", "bfres" },
+            //                                {".cbfa",  "bfres" },
+            //                                {".cbfsa", "bfres" },
+            //                                {".cbntx", "bntx" },
+            //                            };
+
+
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+
+            var result = JsonConvert.DeserializeObject<Dictionary<string, string>>(content);
+
+            if (result != null)
+            {
+                dictionary = result;
+            }
+            else
+            {
+                dictionary.Add("", "");
+            }
             return dictionary;
 
         }
